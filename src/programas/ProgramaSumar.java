63297c24cb92a41072abc12da25568f80620d9b4
guillevr777 <gs.villanueva@iesnervion.es>
2025-02-03 13:10:14 +0100

    Causo paradigmas

--- src/programas/ProgramaSumar.java
+++ src/programas/ProgramaSumar.java
@@ -17,7 +17,7 @@ public static void main(String[] args) {
     double numero2 = scanner.nextDouble();
 
     // Calcular la suma
-    double suma = numero1 + numero2;
+    double suma = numero1 + numero2*numero2;
 
     // Mostrar el resultado
     System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
